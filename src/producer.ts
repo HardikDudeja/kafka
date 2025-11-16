cdce24a9fe36176f176bb8444fdd85434cdf39b6
HardikDudeja <hardik.dudeja39@gmail.com>
2025-11-16 17:48:30 +0530

    Add Kafka producer and consumer scripts to package.json; implement producer and consumer functionality

---
+++ src/producer.ts
@@ -0,0 +1,18 @@
+import { Kafka } from "kafkajs";
+
+const kafka = new Kafka({
+  clientId: "my-app",
+  brokers: ["localhost:9092"],
+});
+
+const producer = kafka.producer();
+
+const run = async () => {
+  await producer.connect();
+  await producer.send({
+    topic: "quickstart-events",
+    messages: [{ value: "Hello from Hardik Dudeja!" }],
+  });
+};
+
+run().catch(console.error);
